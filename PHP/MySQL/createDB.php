96d093e503252302af47c8cec60272171540020d
neel-yudiz <neel.patel@yudiz.com>
2023-04-22 16:40:33 +0530

    Completed till Adding multiple entries (MySQL)

--- PHP/MySQL/createDB.php
+++ PHP/MySQL/createDB.php
@@ -13,7 +13,8 @@ if (! $conn) {
 echo "Connected !!";
 
 //After successfull connection
-//Creating database - Yudiz
+
+//CREATING DB 'YUDIZ'
 // $conn = "CREATE DATABASE Yudiz";
 
 // if ($conn -> query($conn) == true) {
@@ -22,13 +23,47 @@ echo "Connected !!";
 //     echo "ERROR !!" . $conn -> error;
 // }
 
-//Creating table - trainee
-$sql = "CREATE TABLE yudiz. ( 'id' INT(3) NOT NULL AUTO_INCREMENT , 'EmployeeID' BIGINT(255) NOT NULL , 'Name' VARCHAR(255) NOT NULL , 'Designation' VARCHAR(255) NOT NULL , PRIMARY KEY ('id'))";
+// SELECT DB THEN CREATE TABLE
+$conn->select_db("yudiz");
 
-if ($conn -> query($sql) == true) {
-    echo "Table created successfully";
+// CREATING TABLE NAMED AS 'TRAINEES'
+// $sql = "CREATE TABLE `trainees` (
+//     `id` INT(3) NOT NULL AUTO_INCREMENT,
+//     `EmployeeID` BIGINT(255) NOT NULL,
+//     `fullName` VARCHAR(255) NOT NULL,
+//     `Designation` VARCHAR(255) NOT NULL,
+//     PRIMARY KEY (`id`))";
+
+// if ($conn -> query($sql) == true) {
+//     echo "Table created successfully";
+// } else {
+//     echo "Error!! Table is not created" . $conn -> error;
+// }
+
+
+//INSERTING DATA INTO THE 'TRAINEES'
+// $sql = "INSERT INTO `trainees` (EmployeeID, fullName, Designation) VALUES ('2033', 'Neel Patel', 'Jr. Web Developer')";
+
+// if ($conn -> query($sql) == true) {
+//     $last_id = $conn -> insert_id;
+//     echo "Values added successfully. Last insertion ID is:" . $last_id ;
+// } else {
+//     echo "Error! Values are not added !!";
+// }
+
+
+//IF THERE ARE MULTIPLE ENTRIES TO BE ADDED
+$sql = "INSERT INTO `trainees` (EmployeeID, fullName, Designation) VALUES ('2033', 'Neel Patel', 'Jr. Web Developer');";
+$sql .= "INSERT INTO `trainees` (EmployeeID, fullName, Designation) VALUES ('2034', 'Kaushal Gandhi', 'Jr. Web Designer');";
+$sql .= "INSERT INTO `trainees` (EmployeeID, fullName, Designation) VALUES ('2035', 'Parth Thakkar', 'Jr. Game Developer');";
+
+if ($conn -> multi_query($sql) === true) {
+    $last_id = $conn -> insert_id;
+    echo "Values added successfully. Last insertion ID is:" . $last_id ;
 } else {
-    echo "Error!! Table is not created" . $conn -> error;
+    echo "Error! Values are not added !!";
 }
+
+
 $conn -> close();
 ?>
