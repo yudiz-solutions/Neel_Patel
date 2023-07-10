49b1c250bd59c02ef18248453d954d07f744b936
neel-yudiz <neel.patel@yudiz.com>
2023-07-10 19:17:34 +0530

    Popular Post completed | Infinite loop in Recommended Post

--- WordPress/wp-expatriate-task/wp-content/themes/expatriate/functions.php
+++ WordPress/wp-expatriate-task/wp-content/themes/expatriate/functions.php
@@ -490,3 +490,41 @@ function load_more_news()
 }
 add_action('wp_ajax_load_more_news', 'load_more_news');
 add_action('wp_ajax_nopriv_load_more_news', 'load_more_news');
+
+//---------------------------------------------------------------------------//
+
+//== POPULAR POST BY VIEWS ==//
+
+function set_post_views($post_id)
+{
+    $count_key = 'wp_post_views_count';
+    $count = get_post_meta($post_id, $count_key, true);
+
+    if ($count == "") {
+        $count = 0;
+        delete_post_meta($post_id, $count_key);
+        add_post_meta($post_id, $count_key, '0');
+    } else {
+        $count++;
+        update_post_meta($post_id, $count_key, $count);
+    }
+}
+
+function track_post_views($post_id)
+{
+    if (!is_single())
+        return;
+
+    if (empty($post_id)) {
+        global $post;
+        $post_id = $post->ID;
+    }
+
+    set_post_views($post_id);
+}
+add_action('wp_head', 'track_post_views');
+remove_action('wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0);
+
+//---------------------------------------------------------------------------//
+
+//== RECOMMENDED POST BY DATE ==//
--- WordPress/wp-expatriate-task/wp-content/themes/expatriate/single-news.php
+++ WordPress/wp-expatriate-task/wp-content/themes/expatriate/single-news.php
@@ -4,9 +4,6 @@ $news_banner_grp = get_field('news_post_grp', 'option');
 $news_pt_grp = get_field('news_pt_grp');
 $temp = get_field('news_grp');
 global $post;
-// echo "<pre>";
-// print_r($post);
-// echo "</pre>";
 ?>
 
 <!--========= BANNER SECTION STARTS =========-->
@@ -75,11 +72,12 @@ global $post;
                             </div>
                             <div class="clearfix"></div>
                         </div>
-                        <div class="articles-content">
-                            <?php
-                            echo $news_pt_grp['post_content'];
-                            ?>
-                        </div>
+                        <?php
+                        if (!empty($news_pt_grp['post_content'])) { ?>
+                            <div class="articles-content">
+                                <?php echo $news_pt_grp['post_content']; ?>
+                            </div>
+                        <?php } ?>
                         <div class="articles-footer-meta">
                             <div class="row">
                                 <div class="col-md-6">
@@ -111,96 +109,94 @@ global $post;
                             <li><a data-toggle="tab" href="#recommended">Recommended</a></li>
                         </ul>
                         <div class="tab-content">
+
+                            <!-- ////////// POPULAR POST BY VIEWS //////////////// -->
+
                             <div id="popular" class="tab-pane fade in active">
-                                <div class="article-tab-item">
-                                    <div class="article-img">
-                                        <img class="full-img" src="images/article-img.jpg" alt="article-img">
-                                    </div>
-                                    <div class="article-desc">
-                                        <h4><a href="article-detail.html">XsunX, Inc. (OTC Pink: XSNX) Surges 150% after
-                                                Announcing Marketing Efforts are Delivering…</a></h4>
-                                        <p class="item-meta"><span class="category">Press Release</span> Jun 4, 2018
-                                        </p>
-                                    </div>
-                                    <div class="clearfix"></div>
-                                </div>
-                                <div class="article-tab-item">
-                                    <div class="article-img">
-                                        <img class="full-img" src="images/article-img1.jpg" alt="article-img">
-                                    </div>
-                                    <div class="article-desc">
-                                        <h4><a href="article-detail.html">XsunX, Inc. (OTC Pink: XSNX) Surges 150% after
-                                                Announcing Marketing Efforts are Delivering…</a></h4>
-                                        <p class="item-meta"><span class="category">Press Release</span> Jun 4, 2018
-                                        </p>
-                                    </div>
-                                    <div class="clearfix"></div>
-                                </div>
-                                <div class="article-tab-item">
-                                    <div class="article-img">
-                                        <img class="full-img" src="images/article-img2.jpg" alt="article-img">
-                                    </div>
-                                    <div class="article-desc">
-                                        <h4><a href="article-detail.html">XsunX, Inc. (OTC Pink: XSNX) Surges 150% after
-                                                Announcing Marketing Efforts are Delivering…</a></h4>
-                                        <p class="item-meta"><span class="category">Press Release</span> Jun 4, 2018
-                                        </p>
-                                    </div>
-                                    <div class="clearfix"></div>
-                                </div>
-                                <div class="article-tab-item">
-                                    <div class="article-img">
-                                        <img class="full-img" src="images/article-img3.jpg" alt="article-img">
-                                    </div>
-                                    <div class="article-desc">
-                                        <h4><a href="article-detail.html">XsunX, Inc. (OTC Pink: XSNX) Surges 150% after
-                                                Announcing Marketing Efforts are Delivering…</a></h4>
-                                        <p class="item-meta"><span class="category">Press Release</span> Jun 4, 2018
-                                        </p>
-                                    </div>
-                                    <div class="clearfix"></div>
-                                </div>
-                                <div class="article-tab-item">
-                                    <div class="article-img">
-                                        <img class="full-img" src="images/article-img4.jpg" alt="article-img">
-                                    </div>
-                                    <div class="article-desc">
-                                        <h4><a href="article-detail.html">XsunX, Inc. (OTC Pink: XSNX) Surges 150% after
-                                                Announcing Marketing Efforts are Delivering…</a></h4>
-                                        <p class="item-meta"><span class="category">Press Release</span> Jun 4, 2018
-                                        </p>
-                                    </div>
-                                    <div class="clearfix"></div>
-                                </div>
+                                <?php
+                                $popular_posts_args = array(
+                                    'post_type' => 'news',
+                                    'meta_key' => 'wp_post_views_count',
+                                    'orderby' => 'meta_value_num',
+                                    'order' => 'DESC',
+                                    'posts_per_page' => '5'
+                                );
+
+                                $popular_posts = new WP_Query($popular_posts_args);
+
+                                if ($popular_posts->have_posts()) {
+                                    while ($popular_posts->have_posts()) {
+                                        $popular_posts->the_post();
+                                        ?>
+                                        <div class="article-tab-item">
+                                            <div class="article-img">
+                                                <img class="full-img" src="<?php echo get_the_post_thumbnail_url(); ?>"
+                                                    alt="article-img">
+                                            </div>
+                                            <div class="article-desc">
+                                                <h4>
+                                                    <a href="<?php echo get_the_permalink(); ?>">
+                                                        <?php echo get_the_excerpt(); ?>
+                                                    </a>
+                                                </h4>
+                                                <p class="item-meta">
+                                                    <span class="category">
+                                                        <?php echo (get_the_terms(get_the_ID(), 'news-category'))[0]->name; ?>
+                                                    </span>
+                                                    <?php echo get_the_date('M j, Y', get_the_ID()); ?>
+                                                </p>
+                                            </div>
+                                            <div class="clearfix"></div>
+                                        </div>
+                                        <?php
+                                    }
+                                    wp_reset_postdata();
+                                }
+                                ?>
                             </div>
 
-                            <!-- ////////////////////////// -->
+                            <!-- ////////// RECOMMENDED //////////////// -->
 
                             <div id="recommended" class="tab-pane fade">
-                                <div class="article-tab-item">
-                                    <div class="article-img">
-                                        <img class="full-img" src="images/article-img2.jpg" alt="article-img">
-                                    </div>
-                                    <div class="article-desc">
-                                        <h4><a href="article-detail.html">XsunX, Inc. (OTC Pink: XSNX) Surges 150% after
-                                                Announcing Marketing Efforts are Delivering…</a></h4>
-                                        <p class="item-meta"><span class="category">Press Release</span> Jun 4, 2018
-                                        </p>
-                                    </div>
-                                    <div class="clearfix"></div>
-                                </div>
-                                <div class="article-tab-item">
-                                    <div class="article-img">
-                                        <img class="full-img" src="images/article-img3.jpg" alt="article-img">
-                                    </div>
-                                    <div class="article-desc">
-                                        <h4><a href="article-detail.html">XsunX, Inc. (OTC Pink: XSNX) Surges 150% after
-                                                Announcing Marketing Efforts are Delivering…</a></h4>
-                                        <p class="item-meta"><span class="category">Press Release</span> Jun 4, 2018
-                                        </p>
-                                    </div>
-                                    <div class="clearfix"></div>
-                                </div>
+                                <?php
+                                $recommended_post_args = array(
+                                    'post_type' => 'news',
+                                    'numberposts' => '4',
+                                    'orderby' => 'modified',
+                                    'order' => 'ASC',
+                                );
+
+                                $recommended_post = new WP_Query($recommended_post_args);
+
+                                if ($recommended_post->have_posts()) {
+                                    while ($recommended_post->have_posts()) {
+                                        $popular_posts->the_post();
+                                        ?>
+                                        <div class="article-tab-item">
+                                            <div class="article-img">
+                                                <img class="full-img" src="<?php echo get_the_post_thumbnail_url(); ?>"
+                                                    alt="article-img">
+                                            </div>
+                                            <div class="article-desc">
+                                                <h4>
+                                                    <a href="<?php echo get_the_permalink(); ?>">
+                                                        <?php echo get_the_excerpt(); ?>
+                                                    </a>
+                                                </h4>
+                                                <p class="item-meta">
+                                                    <span class="category">
+                                                        <?php echo (get_the_terms(get_the_ID(), 'news-category'))[0]->name; ?>
+                                                    </span>
+                                                    <?php echo get_the_date('M j, Y', get_the_ID()); ?>
+                                                </p>
+                                            </div>
+                                            <div class="clearfix"></div>
+                                        </div>
+                                        <?php
+                                    }
+                                    wp_reset_postdata();
+                                }
+                                ?>
                             </div>
                         </div>
                     </div>
